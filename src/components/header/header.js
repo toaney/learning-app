ef29ff6628bd8ae4ce1cce9c7cff45abbc2acb2b
Maj Toan <toaney@gmail.com>
2020-02-05 12:24:32 -0800

    add header component

---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './header.scss';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return(
+        <header>
+            <div className="nav-container">
+            {/* <img className="header-logo" src={"../../assets/images/ESLLLogo.webp"} alt="espark logo" /> */}
+            <div className="header-logo"><Link className="more-articles-link" to="/">eSpark</Link></div>
+                <nav>
+                    <span className="header-nav-list">
+                        <span className="header-nav-link"><Link className="more-articles-link" to="/hyp1">hypothesis1</Link></span>
+                        <span className="header-nav-link"><Link className="more-articles-link" to="/hyp2">hypothesis2</Link></span>
+                        <span className="header-nav-link"><Link className="more-articles-link" to="/hyp3">hypothesis3</Link></span>
+                    </span>
+                </nav>
+            </div>
+            <aside className="header-login-container">
+                {/* header-login-link is purely cosmetic and won't work */}
+                <span className="header-login-link"></span>
+            </aside>
+        </header>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './scss/styles.scss';
 import Home from './pages/home';
 import Hyp1 from './pages/hyp1';
 import Hyp2 from './pages/hyp2';
 import Hyp3 from './pages/hyp3';
+import Header from './components/header/header';
 
 ReactDOM.render(
     <Router>
+      <Header />
       <Switch>
         <Route path="/hyp1" component={ Hyp1 } />
         <Route path="/hyp2" component={ Hyp2 } />
